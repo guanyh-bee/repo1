e01d43b192c29de71f92dae6c103dffe7bcfcb97
gyh <543841473@qq.com>
2020-06-29 13:22:31 +0800

    添加Java工程到本地仓库

---
+++ restfulcrud/src/test/java/com/gyh/restfulcrud/RestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyh.restfulcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
